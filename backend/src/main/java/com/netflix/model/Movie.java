e28b8e1140299bfd488193dcf666ce9d79804a8b
Nishanth-Vijay-vj <nishanthvijayrj@gmail.com>
2026-04-29 00:05:18 +0530

    Update Movie.java

--- backend/src/main/java/com/netflix/model/Movie.java
+++ backend/src/main/java/com/netflix/model/Movie.java
@@ -2,13 +2,12 @@ package com.netflix.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
-@Data
+@Entity @Data
 public class Movie {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private String genre;
-    private Double rating;
+    private String description;
+    private String posterPath; // To store image URL
+    private String category;   // Action, Trending, etc.
 }
