6ced58d6cd6fec111715c71d564ec74852ff10ea
Nishanth-Vijay-vj <nishanthvijayrj@gmail.com>
2026-04-29 00:30:30 +0530

    Update movie.service.ts

--- frontend/src/app/movie.service.ts
+++ frontend/src/app/movie.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 export class MovieService {
   // Use your Master's Public IP
-  private apiUrl = 'http://13.206.86';
+  private apiUrl = 'http://15.206.27.63';
 
   constructor(private http: HttpClient) { }
 
